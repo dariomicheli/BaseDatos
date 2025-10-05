c470cb5a4f722b8728ce27c4b742c584d0432abe
Pablo Luberriaga <pablolube@example.com>
2025-10-05 12:48:59 -0300

    Creo version de cargar en neo4j

---
+++ notebooks/src/neo4j.py
@@ -0,0 +1,22 @@
+def crear_nodo(tx, nombre_nodo, clave_id, datos):
+    """
+    Crea un nodo en Neo4j de forma genérica si no existe.
+
+    Parámetros:
+    - tx: transacción Neo4j
+    - label: etiqueta del nodo (string)
+    - clave_id: nombre del campo único (string)
+    - datos: diccionario con las propiedades del nodo
+    """
+    query = f"""
+    MERGE (n:{nombre_nodo} {{{clave_id}: $valor_id}})
+    ON CREATE SET
+        n += $propiedades
+    RETURN n
+    """
+    result = tx.run(
+        query,
+        valor_id=datos[clave_id],
+        propiedades=datos
+    )
+    return result.single()
