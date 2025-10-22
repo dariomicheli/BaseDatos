5508143b41b68d1771eef34735e601d4f41b697d
Pablo Luberriaga <pablolube@example.com>
2025-10-22 17:31:21 -0300

    Ajusto carga de datos

--- notebooks/src/neo4j.py
+++ notebooks/src/neo4j.py
@@ -3,6 +3,10 @@ from pathlib import Path
 from db_connections import db_neo4j
 from src import utils,neo4j
 
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#                                               Creacicion de Nodos
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 def nodo_existe(label, driver):
     """
     Verifica si existen nodos de un tipo específico en Neo4j.
@@ -42,6 +46,48 @@ def crear_nodo(tx, nombre_nodo, clave_id, datos):
     )
     return result.single()
 
+
+def crear_nodos_neo4j(nombre_coleccion, df):
+    """
+    Crea nodos en Neo4j a partir de los datos de una colección específica (usuarios o destinos).
+
+    Esta función toma un DataFrame de pandas y genera nodos en una base de datos Neo4j,
+    diferenciando entre las colecciones de "usuarios" y "destinos". Cada fila del DataFrame
+    se convierte en un nodo con sus propiedades correspondientes.
+
+    Args:
+        nombre_coleccion (str): Nombre de la colección que se desea procesar.
+            Debe ser "usuarios" o "destinos".
+        df (pandas.DataFrame): DataFrame que contiene los datos a insertar.
+            - Para "usuarios", se esperan las columnas: ["usuario_id", "nombre", "apellido"].
+            - Para "destinos", se esperan las columnas: ["destino_id", "provincia", "ciudad"].
+
+    Returns: none
+    """
+    if nombre_coleccion not in ["usuarios", "destinos"]:
+        return
+
+    # Defino las claves que uso para usuaros y destino
+    nombre_nodo = "Usuario" if nombre_coleccion == "usuarios" else "Destino"
+    campo_clave = "usuario_id" if nombre_nodo == "Usuario" else "destino_id"
+
+    #Filtro los dato que realmente me quiero guardar en Neo4J y lo transformo en un diccionario de listas
+    if nombre_nodo == "Usuario":
+        filas = df[["usuario_id", "nombre", "apellido"]].to_dict("records")
+    else:
+        filas = df[["destino_id", "provincia", "ciudad"]].to_dict("records")
+
+    #Conecto con Neo4j 
+    with db_neo4j.session() as session: 
+        for fila in filas:
+            session.execute_write(neo4j.crear_nodo, nombre_nodo, campo_clave, fila) # Recorro filas y creo nodo invocando a la funcion creo nodo para cada usuarios/destino
+
+    print(f"✅ Nodos de tipo '{nombre_nodo}' creados exitosamente en Neo4j.")
+
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#                                               Creación de Relaciones
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 def crear_relacion_unidireccional(tx, nodo_origen, campo_origen, valor_origen,
                    nodo_destino, campo_destino, valor_destino,
                    tipo_relacion):
@@ -68,47 +114,6 @@ def crear_relacion_bidireccional(tx, nodo_origen, campo_origen, valor_origen,
         valor_origen=valor_origen,
         valor_destino=valor_destino
     ).single()
-
-def consulta(db, query, parametros=None):
-    """
-    Ejecuta una query en Neo4j y devuelve los resultados como un DataFrame.
-
-    Parámetros:
-        db: objeto de conexión a Neo4j (db_neo4j)
-        query: string con la consulta Cypher
-        parametros: diccionario con parámetros de la query (opcional)
-
-    Retorna:
-        pd.DataFrame con los resultados
-    """
-    with db.session() as session:
-        resultados = session.run(query, parametros)
-        data = [record.data() for record in resultados]
-    return pd.DataFrame(data)
-
-
-# -------------------------------------------------------------
-# FUNCIONES AUXILIARES
-# -------------------------------------------------------------
-
-def crear_nodos_neo4j(nombre_coleccion, df):
-    """
-    Crea nodos en Neo4j según la colección (usuarios o destinos).
-    """
-    if nombre_coleccion not in ["usuarios", "destinos"]:
-        return
-
-    nombre_nodo = "Usuario" if nombre_coleccion == "usuarios" else "Destino"
-    campo_clave = "usuario_id" if nombre_nodo == "Usuario" else "destino_id"
-
-    filas = df.to_dict("records")
-
-    with db_neo4j.session() as session:
-        for fila in filas:
-            session.execute_write(neo4j.crear_nodo, nombre_nodo, campo_clave, fila)
-
-    print(f"✅ Nodos de {nombre_nodo} creados exitosamente en Neo4j.")
-
 
 def crear_relaciones_visito(df):
     """
@@ -138,7 +143,6 @@ def crear_relaciones_visito(df):
 
     print("✅ Relaciones VISITO creadas exitosamente en Neo4j.")
 
-
 def crear_relaciones_usuarios():
     """
     Crea relaciones bidireccionales entre usuarios (usuarios_relaciones.csv).
@@ -161,6 +165,27 @@ def crear_relaciones_usuarios():
 
     print("✅ Relaciones entre usuarios creadas exitosamente en Neo4j.")
 
+  
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#                                               Consultas
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def consulta(db, query, parametros=None):
+    """
+    Ejecuta una query en Neo4j y devuelve los resultados como un DataFrame.
+
+    Parámetros:
+        db: objeto de conexión a Neo4j (db_neo4j)
+        query: string con la consulta Cypher
+        parametros: diccionario con parámetros de la query (opcional)
+
+    Retorna:
+        pd.DataFrame con los resultados
+    """
+    with db.session() as session:
+        resultados = session.run(query, parametros)
+        data = [record.data() for record in resultados]
+    return pd.DataFrame(data)
 
 def eliminar_amigos(usuario_id):
     """
@@ -178,4 +203,4 @@ def eliminar_amigos(usuario_id):
         resultado = session.run(query,id=usuario_id)
         cantidad = resultado.single()["cantidad"]
         return cantidad
-        
+      
